13622b589c226892ff6ab9399b756f92fe08e4a9
AkhilStaro <101500483+AkhilStaro@users.noreply.github.com>
2022-05-18 15:44:55 +0530

    Add files via upload
    Added Doom Emacs Config

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 colorscript random
+
 end
+
+alias refreshmirrors="reflector --download-timeout 59 -a 48 -c IN -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist"
+alias emacs="emacsclient -c"
+alias doom=".emacs.d/bin/doom"
+alias picomm="picom --experimental-backends"
